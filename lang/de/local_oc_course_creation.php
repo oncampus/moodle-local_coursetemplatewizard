afd878c81811ca542bb91f2d9ed597c34ea0969c
Laurenz Schindler <Laurenz.Schindler@oncampus.de>
2024-08-23 14:20:30 +0200

    changed lang for flawsless logic

--- lang/de/local_oc_course_creation.php
+++ lang/de/local_oc_course_creation.php
@@ -75,7 +75,7 @@ $string['form:copy:description'] =
     'Dieser Kurs wird erstellt und in die angegebene Kurskategorie eingefügt.';
 $string['form:copy:select_default'] = 'Kursbereich auswählen';
 $string['form:copy:prefix_desc'] = 'Modulbezeichnung hinzufügen';
-$string['settings:use_default_course_naming'] = 'Kursname ohne Vorgaben erstellen';
+$string['settings:use_default_course_naming'] = 'Kursname mit Vorgaben erstellen';
 $string['settings:use_default_course_naming_desc'] =
     'Dieses Feld deaktiviert in der Kurserstellungsform alle optionen zur Vorgabe des Kursnamens.';
 $string['settings:template_textfield_values'] = 'Vorgaben für Textfelder';
--- lang/en/local_oc_course_creation.php
+++ lang/en/local_oc_course_creation.php
@@ -75,7 +75,7 @@ $string['form:copy:description'] =
     'This course will be created and added to the specified course category.';
 $string['form:copy:select_default'] = 'Select course category';
 $string['form:copy:prefix_desc'] = 'Add module name';
-$string['settings:use_default_course_naming'] = 'Create course name without preselections';
+$string['settings:use_default_course_naming'] = 'Create course name with preselections';
 $string['settings:use_default_course_naming_desc'] =
     'This field disables all course name template options in the course creation form.';
 $string['settings:template_textfield_values'] = 'Textfield input masks';
